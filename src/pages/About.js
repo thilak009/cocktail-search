c5db6a47ea6cb32b95cb5142725cc360544ee7a2
thilak reddy <thilak009reddy@gmail.com>
2021-06-14 22:16:15 +0530

    content changes

--- src/pages/About.js
+++ src/pages/About.js
@@ -3,14 +3,21 @@ import React from "react";
 export default function About() {
   return (
     <section className="section about-section">
-      <h1 className="section-title">about us</h1>
+      <h1 className="section-title">about</h1>
       <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae
-        repudiandae architecto qui adipisci in officiis, aperiam sequi atque
-        perferendis eos, autem maiores nisi saepe quisquam hic odio consectetur
-        nobis veritatis quasi explicabo obcaecati doloremque? Placeat ratione
-        hic aspernatur error blanditiis?
+        This is a website built using React 
+        and <a href="https://www.thecocktaildb.com/api.php" target="_blank" rel="noreferrer" style={{textDecoration:"underline"}}>CocktailDB API</a> for 
+        displaying different cocktails and their details,buil to learn the react router along with API fetching and content
+        display
       </p>
+      <div style={{display:"flex",flexDirection:"column",alignItems:"center"}}>
+        <p style={{fontSize:"20px"}}>Built by Thilak</p>
+        <div style={{display:"flex",gap:"5px",textDecoration:"underline"}}>
+          <a href="https://twitter.com/thilak_ch"  target="_blank" rel="noreferrer">Twitter</a>
+          <a href="https://github.com/thilak009"  target="_blank" rel="noreferrer">Github</a>
+          <a href="https://www.linkedin.com/in/thilak-reddy-87ab4b1b3/"  target="_blank" rel="noreferrer">Linkedin</a>
+        </div>
+      </div>
     </section>
   );
 }
